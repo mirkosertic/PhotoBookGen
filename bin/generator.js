09e9994ba4b55a6cc2bcf4d16a14c13ce46baaf8
Mirko Sertic <mirko.sertic@web.de>
2018-08-21 22:31:37 +0200

    Tests with other image processing libraries

--- bin/generator.js
+++ bin/generator.js
@@ -2,18 +2,25 @@
 
 var app = require('../app');
 
-var imagemin = require("imagemin");    // The imagemin module
-var webp = require("imagemin-webp");   // imagemin's WebP plugin
+var sharp = require("sharp");
 var debug = require('debug')('PhotoBookGen')
 
 var outputFolder = "D:\\IdeaProjects\\PhotoBookGen\\generated\\images";            // Output folder
-var JPEGImages = "D:\\IdeaProjects\\PhotoBookGen\\generated\\source\\*.jpg";        // JPEG images
+var JPEGImages = "D:\\owncloud_hochzeit\\Hochzeit\\Fotos\\*.jpg";        // JPEG images
 
 console.log("Generating WebP files in  " +  outputFolder);
 
-imagemin([JPEGImages], outputFolder, {
-    plugins: [webp({
-        quality: 75 // Quality setting from 0 to 100
-    })]
-});
+sharp("D:\\IdeaProjects\\PhotoBookGen\\generated\\source\\Hochzeit_22.07.2018-1.jpg")
+    .toFile("D:\\IdeaProjects\\PhotoBookGen\\generated\\images\\large.webp");
 
+sharp("D:\\IdeaProjects\\PhotoBookGen\\generated\\source\\Hochzeit_22.07.2018-1.jpg")
+    .resize(400)
+    .toFile("D:\\IdeaProjects\\PhotoBookGen\\generated\\images\\small.webp");
+
+sharp("D:\\IdeaProjects\\PhotoBookGen\\generated\\source\\Hochzeit_22.07.2018-1.jpg")
+    .resize(400)
+    .jpeg({
+        quality: 90,
+        progressive: true
+    })
+    .toFile("D:\\IdeaProjects\\PhotoBookGen\\generated\\images\\small.jpeg");
